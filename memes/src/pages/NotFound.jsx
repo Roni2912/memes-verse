f5e81887bb01e0732bd37c32ceccfcb6ce77f2eb
ronishdudhat <Ronish@limbanisoftwares.com>
2025-03-08 11:07:09 +0530

    refactor code structure for improved readability and maintainability

--- memes/src/pages/NotFound.jsx
+++ memes/src/pages/NotFound.jsx
@@ -64,7 +64,7 @@ function NotFound() {
           className="mb-12 relative max-w-sm mx-auto"
         >
           <img
-            src="../public/images/err.jpg" // Replace with your fun image
+            src=".././public/images/4.jpg" 
             alt="Lost in space"
             className="w-full h-auto rounded-xl shadow-lg"
           />
